bb3b9605949fb36e22a350a216f549e692340843
Lena <lenazun@gmail.com>
2014-07-27 14:49:09 -0400

    Learn Python the Hard Way
    First batch of excercises

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+
+script, first, second = argv
+third = raw_input("what's the third arg? ")
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
